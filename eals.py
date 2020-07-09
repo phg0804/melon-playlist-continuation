0f6a8be381674d0c8a870de0e88daea4d8cb9a77
Hyungyu Park <hyungyupark@Hyungyus-MacBookPro.local>
2020-07-10 02:11:50 +0900

    slow version

--- eals.py
+++ eals.py
@@ -5,206 +5,159 @@ import time
 from collections import Counter
 import numba as nb
 from numba import njit
-from numba import jitclass
-from numba import int32, float32, types, boolean
 
-'''
-genre = au.load_json("./res/genre_gn_all.json")
-song = au.load_json("./res/song_meta.json")
+def read_mm(fname):
+    f = open(fname, 'r')
+    r, c, n = f.readline().split()
+    global row, prev_row, col, N, prev_N
+    prev_row = row
+    row += int(r)
+    col = int(c)
+    prev_N = int(N)
+    N += int(n)
+    data_user.resize(N)
+    data_item.resize(N)
+    data_score.resize(N)
+    for i in range(prev_N, N):
+        data_user[i], data_item[i], data_score[i] = f.readline().split()
+        data_user[i] += prev_row
+    f.close()
 
-train_virtual = au.load_json("./arena_data/orig/train.json")
-val_virtual = au.load_json("./arena_data/questions/val.json")
-train = au.load_json("./res/train.json")
-val = au.load_json("./res/val.json")
-'''
+#@njit
+def getRhat():
+    for i in range(N):
+        r, c = data_user[i], data_item[i]
+        rui_hat[r].append([P[r].dot(Q[c]), c, -1])
+        riu_hat[c].append([rui_hat[r][col_num[r]][0], r, -1])
+        rui_hat[r][-1][2] = row_num[c]
+        riu_hat[c][-1][2] = col_num[r]
+        row_num[c] += 1
+        col_num[r] += 1
 
-'''
-spec_MatrixCoo = [
-        ('row', int32), 
-        ('col', int32), 
-        ('prev_N', int32), 
-        ('N', int32), 
-        ('data_user', int32[:]),
-        ('data_item', int32[:]),
-        ('data_score', float32[:]),
-]
-'''
+#@njit
+def checkConvergence():
+    global assigned, P_current, Q_current
+    diff_P = diff_Q = -1
+    if assigned:
+        diff_P = np.linalg.norm(P - P_current)
+        diff_Q = np.linalg.norm(Q - Q_current)
+        print("Difference(Frobenius norm) - \
+              P : %.10f, Q : %.10f" % (diff_P, diff_Q))
+    P_current = P.copy()
+    Q_current = Q.copy()
+    assigned = True
+    return max(diff_P, diff_Q)
 
-class MatrixCoo:
-    def __init__(self):
-        self.row = self.col = self.N = self.prev_N = 0
-        self.data_user = np.array([], dtype=np.int32)
-        self.data_item = np.array([], dtype=np.int32)
-        self.data_score = np.array([], dtype=np.float32)
+#@njit
+def train_epoch():
+    #train user factors
+    starttime = time.time()
+    Sq = Q.T.dot(Q)
+    for u in range(num_user):
+        for f in range(num_factor):
+            rui_hat_f = np.zeros(col_num[u], dtype=np.float32)
+            numerator = P[u][f] * Sq[f][f] - P[u].dot(Sq[f])
+            denominator = Sq[f][f] + ld
+            for i_ind in range(col_num[u]):
+                qif = Q[rui_hat[u][i_ind][1]][f]
+                rui_hat_f[i_ind] = rui_hat[u][i_ind][0] - P[u][f] * qif
+                numerator += (1 + confidence - confidence * rui_hat_f[i_ind]) * qif
+                denominator += confidence * qif * qif
+            P[u][f] = numerator / denominator
+            for i_ind in range(col_num[u]):
+                qif = Q[rui_hat[u][i_ind][1]][f]
+                rui_hat[u][i_ind][0] = P[u][f] * qif + rui_hat_f[i_ind]
+                c, u_ind = rui_hat[u][i_ind][1:]
+                riu_hat[c][u_ind][0] = rui_hat[u][i_ind][0]
+    print("Updating P took %.2fs" % (time.time() - starttime))
 
-    def read(self, fname):
-        f = open(fname, 'r')
-        r, c, n = f.readline().split()
-        self.row += int(r)
-        self.col = int(c)
-        self.prev_N = self.N
-        self.N += int(n)
-        self.data_user.resize(self.N)
-        self.data_item.resize(self.N)
-        self.data_score.resize(self.N)
-        for i in range(self.prev_N, self.N):
-            self.data_user[i], self.data_item[i], self.data_score[i] = f.readline().split()
-        f.close()
-
-'''
-spec_eALS = [
-        ('num_factor', int32), 
-        ('max_epoch', int32), 
-        ('ld', float32), 
-        ('confidence', float32), 
-        ('diff_threshold', float32), 
-        ('verbose', boolean), 
-        ('assigned', boolean), 
-        ('fname_train', nb.types.string),
-        ('fname_val', nb.types.string),
-        ('coodata', MatrixCoo.class_type.instance_type),
-        ('val_user', int32),
-        ('num_user', int32),
-        ('num_item', int32),
-        ('X', float32[:, :]),
-        ('X_current', float32[:, :]),
-        ('Y', float32[:, :]),
-        ('Y_current', float32[:, :]),
-]
-'''
-
-class eALS:
-    def __init__(self, _num_factor, _max_epoch, _ld, _confidence, _diff_threshold, _verbose, _fname_train, _fname_val):
-        self.num_factor = _num_factor
-        self.max_epoch = _max_epoch
-        self.ld = _ld
-        self.confidence = _confidence
-        self.diff_threshold = _diff_threshold
-        self.verbose = _verbose
-        self.fname_train = _fname_train
-        self.fname_val = _fname_val
-        self.coodata = MatrixCoo()
-        self.assigned = False
-
-    def loadData(self):
-        self.coodata.read(self.fname_train)
-        num_tmp = self.coodata.row
-        self.coodata.read(self.fname_val)
-        self.val_user = self.coodata.row - num_tmp
-        self.num_user = self.coodata.row
-        self.num_item = self.coodata.col
-        print("Matrix density : {}".format(self.coodata.N / self.num_user / self.num_item, ".8f"))
-        print("N : {}".format(self.coodata.N))
-        print("# of users : {}, # of items : {}".format(self.num_user, self.num_item))
-
-    def train(self):
-        self.X = np.random.rand(self.num_user, self.num_factor)
-        self.X_current = np.random.rand(self.num_user, self.num_factor)
-        self.Y = np.random.rand(self.num_item, self.num_factor)
-        self.Y_current = np.random.rand(self.num_item, self.num_factor)
-        self.getRhat()
-        for i in range(self.max_epoch):
-            print("Epoch {} start".format(i+1))
-            self.train_epoch()
-            diff = self.checkConvergence()
-            if(self.verbose):
-                if(diff < self.diff_threshold):
-                    print("\nEpoch : {}".format(i+1))
-                    print("X and Y converged.")
-                    break
-                if(i == self.max_epoch - 1):
-                    print("\nEpoch : {}".format(i+1))
-                    print("Reached maximum iteration")
-
-    def getRhat(self):
-        self.rui_hat = [None] * self.num_user
-        self.col_num = [0] * self.num_user
-        self.riu_hat = [None] * self.num_item
-        self.row_num = [0] * self.num_item
-        for i in range(self.coodata.N):
-            r, c = self.coodata.data_user[i], self.coodata.data_item[i]
-            if(self.rui_hat[r] == None):
-                self.rui_hat[r] = [[self.X[r].dot(self.Y[c]), c, None]]
-            else:
-                self.rui_hat[r].append([self.X[r].dot(self.Y[c]), c, None])
-            if(self.riu_hat[c] == None):
-                self.riu_hat[c] = [[self.rui_hat[r][self.col_num[r]][0], r, None]]
-            else:
-                self.riu_hat[c].append([self.rui_hat[r][self.col_num[r]][0], r, None])
-            self.rui_hat[r][self.col_num[r]][2] = (c, self.row_num[c])
-            self.riu_hat[c][self.row_num[c]][2] = (r, self.col_num[r])
-            self.row_num[c] += 1
-            self.col_num[r] += 1
-
-    def checkConvergence(self):
-        if self.assigned:
-            diff_X = np.linalg.norm(self.X - self.X_current)
-            diff_Y = np.linalg.norm(self.Y - self.Y_current)
-            print("Difference(Frobenius norm square) - \
-                  X : {}, Y : {}".format(diff_X**2, diff_Y**2, ".6f"))
-        self.X_current = self.X
-        self.Y_current = self.Y
-        self.assigned = True
-        return max(diff_X**2, diff_Y**2)
-
-    def train_epoch(self):
-        #train user factors
-        starttime = time.time()
-        Sq = self.Y.T.dot(self.Y)
-        for u in range(self.num_user):
-            for f in range(self.num_factor):
-                rui_hat_f = np.zeros(self.col_num[u], dtype=np.float32)
-                numerator = self.X[u][f] * Sq[f][f] - self.X[u].dot(Sq[f])
-                denominator = Sq[f][f] + self.ld
-                for i_ind in range(self.col_num[u]):
-                    qif = self.Y[self.rui_hat[u][i_ind][1]][f]
-                    rui_hat_f[i_ind] = self.rui_hat[u][i_ind][0] - self.X[u][f] * qif
-                    denominator += self.confidence * qif * qif
-                self.X[u][f] = numerator / denominator
-                for i_ind in range(self.col_num[u]):
-                    qif = self.Y[self.rui_hat[u][i_ind][1]][f]
-                    self.rui_hat[u][i_ind][0] = self.X[u][f] * qif + rui_hat_f[i_ind]
-        for u in range(self.num_user):
-            for i_ind in range(self.col_num[u]):
-                i, j = self.rui_hat[u][i_ind][2]
-                self.riu_hat[i][j] = self.rui_hat[u][i_ind][0]
-        print("Updating X took {}s".format(time.time() - starttime, ".2f"))
-        # train item factors
-        starttime = time.time()
-        Sp = self.X.T.dot(self.X)
-        for i in range(self.num_item):
-            for f in range(self.num_factor):
-                riu_hat_f = np.zeros(self.row_num[i], dtype=np.float32)
-                numerator = self.Y[i][f] * Sp[f][f] - self.Y[i].dot(Sp[f])
-                denominator = Sp[f][f] + self.ld
-                for u_ind in range(self.row_num[i]):
-                    puf = self.X[self.riu_hat[i][u_ind][1]][f]
-                    riu_hat_f[u_ind] = self.riu_hat[i][u_ind][0] - self.Y[i][f] * puf
-                    denominator += self.confidence * puf * puf
-                self.Y[i][f] = numerator / denominator
-                for u_ind in range(self.row_num[i]):
-                    puf = self.X[self.riu_hat[i][u_ind][1]][f]
-                    self.riu_hat[i][u_ind][0] = self.Y[i][f] * puf + riu_hat_f[u_ind]
-        for i in range(self.num_item):
+    # train item factors
+    starttime = time.time()
+    Sp = P.T.dot(P)
+    for i in range(num_item):
+        for f in range(num_factor):
+            riu_hat_f = np.zeros(row_num[i], dtype=np.float32)
+            numerator = Q[i][f] * Sp[f][f] - Q[i].dot(Sp[f])
+            denominator = Sp[f][f] + ld
             for u_ind in range(row_num[i]):
-                i, j = self.riu_hat[i][u_ind][2]
-                self.rui_hat[i][j] = self.riu_hat[i][u_ind][0]
-        print("Updating Y took {}s".format(time.time() - starttime, ".2f"))
+                puf = P[riu_hat[i][u_ind][1]][f]
+                riu_hat_f[u_ind] = riu_hat[i][u_ind][0] - Q[i][f] * puf
+                numerator += (1 + confidence - confidence * riu_hat_f[u_ind]) * puf
+                denominator += confidence * puf * puf
+            Q[i][f] = numerator / denominator
+            for u_ind in range(row_num[i]):
+                puf = P[riu_hat[i][u_ind][1]][f]
+                riu_hat[i][u_ind][0] = Q[i][f] * puf + riu_hat_f[u_ind]
+                r, i_ind = riu_hat[i][u_ind][1:]
+                rui_hat[r][i_ind][0] = riu_hat[i][u_ind][0]
+    print("Updating Q took %.2fs" % (time.time() - starttime))
 
+if __name__ == "__main__":
 
-cfg = au.load_json('./cfg.json')
-eals = eALS(cfg['num_factor'], cfg['max_epoch'], cfg['lambda'], cfg['confidence'], \
-            cfg['diff_threshold'], cfg['verbose'], cfg['fname_train'], cfg['fname_val'])
-eals.loadData()
-eals.train()
+    # hyperparameter setting
+    cfg = au.load_json('./cfg.json')
 
-f = open('./userpy.txt', 'w')
-for i in range(eals.num_user - eals.val_user, eals.num_user):
-    logit = eals.X[i]
-    f.write(logit)
-f.close()
-f = open('./songpy.txt', 'w')
-for i in range(eals.num_item):
-    logit = eals.Y[i]
-    f.write(logit)
-f.close()
+    # N : number of nonzero elements
+    row = col = N = prev_N = 0
+    data_user = np.array([], dtype=np.int32)
+    data_item = np.array([], dtype=np.int32)
+    data_score = np.array([], dtype=np.float32)
+
+    num_factor = cfg['num_factor']
+    max_epoch = cfg['max_epoch']
+    ld = cfg['lambda']
+    confidence = cfg['confidence']
+    diff_threshold = cfg['diff_threshold']
+    verbose = cfg['verbose']
+    fname_train = cfg['fname_train']
+    fname_val = cfg['fname_val']
+    assigned = False
+
+    read_mm(fname_train)
+    num_user_train = row
+    read_mm(fname_val)
+    num_user_val = row - num_user_train
+    num_user = row
+    num_item = col
+
+    print("Matrix density : {}".format(N / num_user / num_item))
+    print("N : {}".format(N))
+    print("# of users : {}, # of items : {}".format(num_user, num_item))
+
+    # initialize X and Y (-1, 1)
+    P = np.random.rand(num_user, num_factor)
+    P = P * 2 - 1
+    P_current = P
+    Q = np.random.rand(num_item, num_factor)
+    Q = Q * 2 - 1
+    Q_current = Q
+
+    rui_hat = [[] for _ in range(num_user)]
+    col_num = [0] * num_user
+    riu_hat = [[] for _ in range(num_item)]
+    row_num = [0] * num_item
+    getRhat()
+
+    for i in range(max_epoch):
+        print("Epoch {} start".format(i+1))
+        train_epoch()
+        diff = checkConvergence()
+        if(verbose):
+            if(diff > 0 and diff < diff_threshold):
+                print("\nEpoch : {}".format(i+1))
+                print("P and Q converged.")
+                break
+            if(i == max_epoch - 1):
+                print("\nEpoch : {}".format(i+1))
+                print("Reached maximum iteration")
+
+    with open('./user.txt', 'w') as f:
+        for i in range(num_user - num_user_val, num_user):
+            np.savetxt(f, P[i], newline = " ", fmt="%.8lf")
+            f.write("\n")
+    f.close()
+    with open('./item.txt', 'w') as f:
+        for i in range(num_item):
+            np.savetxt(f, Q[i], newline = " ", fmt="%.8lf")
+            f.write("\n")
+    f.close()
